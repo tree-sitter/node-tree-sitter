f84dfaf3c5bdbb3c4f500b8d9dd9cfa734e75f3e
Max Brunsfeld <maxbrunsfeld@gmail.com>
2016-02-02 16:04:48 -0800

    Allow setting the buffer size w/ an argument to setInputString

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ var binding = require('./build/Release/tree_sitter_runtime_binding'),
     Document = binding.Document,
     StringInput = require("./lib/string_input");
 
-Document.prototype.setInputString = function(string) {
+Document.prototype.setInputString = function(string, bufferSize) {
   this.invalidate()
-  this.setInput(new StringInput(string));
+  this.setInput(new StringInput(string, bufferSize));
   return this;
 };
 
