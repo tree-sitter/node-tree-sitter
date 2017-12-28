94c018ca20f428054d684a95c205a7f26945cfb3
Max Brunsfeld <maxbrunsfeld@gmail.com>
2017-12-28 13:16:17 -0800

    Replace 'AstNode' with 'ASTNode' in type declaration file

--- tree-sitter.d.ts
+++ tree-sitter.d.ts
@@ -1,7 +1,7 @@
 declare module "tree-sitter" {
   export class Document {
     // Getters
-    public rootNode: AstNode | null;
+    public rootNode: ASTNode | null;
 
     // Methods
     public setLanguage(module: any): void;
@@ -36,55 +36,55 @@ declare module "tree-sitter" {
     read(): any;
   }
 
-  export interface AstNode {
+  export interface ASTNode {
     // Getters
     id: number;
     isNamed: boolean;
     type: string;
     startPosition: Position;
     endPosition: Position;
-    children: AstNodeArray;
+    children: ASTNodeArray;
     startIndex: number;
     endIndex: number;
-    parent: AstNode | null;
-    namedChildren: AstNodeArray;
-    firstChild: AstNode | null;
-    lastChild: AstNode | null;
-    firstNamedChild: AstNode | null;
-    lastNamedChild: AstNode | null;
-    nextSibling: AstNode | null;
-    nextNamedSibling: AstNode | null;
-    previousSibling: AstNode | null;
-    previousNamedSibling: AstNode | null;
+    parent: ASTNode | null;
+    namedChildren: ASTNodeArray;
+    firstChild: ASTNode | null;
+    lastChild: ASTNode | null;
+    firstNamedChild: ASTNode | null;
+    lastNamedChild: ASTNode | null;
+    nextSibling: ASTNode | null;
+    nextNamedSibling: ASTNode | null;
+    previousSibling: ASTNode | null;
+    previousNamedSibling: ASTNode | null;
 
     // Methods
     isValid(): boolean;
     hasError(): boolean;
     hasChanges(): boolean;
     toString(): string;
-    descendantForIndex(index: number): AstNode;
-    descendantForIndex(startIndex: number, endIndex: number): AstNode;
-    namedDescendantForIndex(index: number): AstNode;
-    namedDescendantForIndex(startIndex: number, endIndex: number): AstNode;
-    descendantForPosition(position: Position): AstNode;
-    descendantForPosition(startPosition: Position, endPosition: Position): AstNode;
-    namedDescendantForPosition(position: Position): AstNode;
-    namedDescendantForPosition(startPosition: Position, endPosition: Position): AstNode;
+    descendantForIndex(index: number): ASTNode;
+    descendantForIndex(startIndex: number, endIndex: number): ASTNode;
+    namedDescendantForIndex(index: number): ASTNode;
+    namedDescendantForIndex(startIndex: number, endIndex: number): ASTNode;
+    descendantForPosition(position: Position): ASTNode;
+    descendantForPosition(startPosition: Position, endPosition: Position): ASTNode;
+    namedDescendantForPosition(position: Position): ASTNode;
+    namedDescendantForPosition(startPosition: Position, endPosition: Position): ASTNode;
   }
 
   type ArrayCallback<TItem, TArray, TReturn> = (node: TItem, index?: number, array?: TArray) => TReturn
 
-  export interface AstNodeArray extends ArrayLike<AstNode>, Iterable<AstNode> {
-    map<T>(callback: ArrayCallback<AstNode, AstNodeArray, void>, thisArg?: any): T[];
-    every(callback: ArrayCallback<AstNode, AstNodeArray, void>, thisArg?: any): void;
-    filter(callback: ArrayCallback<AstNode, AstNodeArray, boolean>, thisArg?: any): AstNode[];
-    find(callback: ArrayCallback<AstNode, AstNodeArray, boolean>, thisArg?: any): AstNode | undefined,
-    findIndex(callback: ArrayCallback<AstNode, AstNodeArray, boolean>, thisArg?: any): number;
-    forEach(callback: ArrayCallback<AstNode, AstNodeArray, void>, thisArg?: any): void;
-    indexOf(searchElement: AstNode, fromIndex?: number): number;
-    reduce<T>(callback: (accumulator: T, current: AstNode, currentIndex?: number, array?: AstNodeArray) => T, initialValue?: T): T;
-    reduceRight<T>(callback: (accumulator: T, current: AstNode, currentIndex?: number, array?: AstNodeArray) => T, initialValue?: T): T;
-    slice(begin?: number, end?: number): AstNode[],
-    some(callback: ArrayCallback<AstNode, AstNodeArray, boolean>, thisArg?: any): boolean,
+  export interface ASTNodeArray extends ArrayLike<ASTNode>, Iterable<ASTNode> {
+    map<T>(callback: ArrayCallback<ASTNode, ASTNodeArray, void>, thisArg?: any): T[];
+    every(callback: ArrayCallback<ASTNode, ASTNodeArray, void>, thisArg?: any): void;
+    filter(callback: ArrayCallback<ASTNode, ASTNodeArray, boolean>, thisArg?: any): ASTNode[];
+    find(callback: ArrayCallback<ASTNode, ASTNodeArray, boolean>, thisArg?: any): ASTNode | undefined,
+    findIndex(callback: ArrayCallback<ASTNode, ASTNodeArray, boolean>, thisArg?: any): number;
+    forEach(callback: ArrayCallback<ASTNode, ASTNodeArray, void>, thisArg?: any): void;
+    indexOf(searchElement: ASTNode, fromIndex?: number): number;
+    reduce<T>(callback: (accumulator: T, current: ASTNode, currentIndex?: number, array?: ASTNodeArray) => T, initialValue?: T): T;
+    reduceRight<T>(callback: (accumulator: T, current: ASTNode, currentIndex?: number, array?: ASTNodeArray) => T, initialValue?: T): T;
+    slice(begin?: number, end?: number): ASTNode[],
+    some(callback: ArrayCallback<ASTNode, ASTNodeArray, boolean>, thisArg?: any): boolean,
   }
 }
