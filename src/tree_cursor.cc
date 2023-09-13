146326f25f732171a6312e0b7e082e098f4e3abf
Ilana Shapiro <ishapiro00@gmail.com>
2023-09-13 13:42:49 -0700

    resolve merge conflict

--- src/tree_cursor.cc
+++ src/tree_cursor.cc
@@ -29,14 +29,14 @@ void TreeCursor::Init(v8::Local<v8::Object> exports) {
   };
 
   FunctionPair methods[] = {
-    {"startPosition", StartPosition},
-    {"endPosition", EndPosition},
+    {"_startPosition", StartPosition},
+    {"_endPosition", EndPosition},
     {"gotoParent", GotoParent},
     {"gotoFirstChild", GotoFirstChild},
     {"gotoFirstChildForIndex", GotoFirstChildForIndex},
     {"gotoNextSibling", GotoNextSibling},
-    {"currentNode", CurrentNode},
-    {"reset", Reset},
+    {"_currentNode", CurrentNode},
+    {"_reset", Reset},
   };
 
   for (size_t i = 0; i < length_of_array(getters); i++) {
