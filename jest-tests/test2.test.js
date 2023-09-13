88ab5842900ea94bef7cd29f32e3a9ff73c98853
Ilana Shapiro <ishapiro00@gmail.com>
2023-09-13 15:26:20 -0700

    add tests

--- jest-tests/test2.test.js
+++ jest-tests/test2.test.js
@@ -6,9 +6,9 @@ jsParser.setLanguage(Javascript);
 describe("Jest test 1", () => {
   it("should work", () => {
     const code = jsParser.parse(`
-    const Parser = require(".");
-    const Javascript = require("tree-sitter-javascript");
-    const jsParser = new Parser();
+      const Parser = require(".");
+      const Javascript = require("tree-sitter-javascript");
+      const jsParser = new Parser();
     `)
     const output = code.rootNode.toString()
     expect(output).toBe('(program (lexical_declaration (variable_declarator name: (identifier) value: (call_expression function: (identifier) arguments: (arguments (string))))) (lexical_declaration (variable_declarator name: (identifier) value: (call_expression function: (identifier) arguments: (arguments (string))))) (lexical_declaration (variable_declarator name: (identifier) value: (new_expression constructor: (identifier) arguments: (arguments)))))');
