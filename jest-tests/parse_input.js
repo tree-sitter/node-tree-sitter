c1d1834ce289d4e0aea602eb0c3ee74507e8ef4c
Amaan Qureshi <amaanq12@gmail.com>
2024-03-04 00:05:51 -0500

    chore: get rid of jest tests

--- jest-tests/parse_input.js
+++
@@ -1,9 +0,0 @@
-const Parser = require("..");
-const Javascript = require("tree-sitter-javascript");
-const jsParser = new Parser();
-jsParser.setLanguage(Javascript);
-
-module.exports = (input) => {
-  const code = jsParser.parse(input)
-  return code.rootNode;
-}
